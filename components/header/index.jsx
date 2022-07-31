e65d05674c56b2cca7e44c5d9b7456117c2a8846
Surma <surma@surma.dev>
2022-07-31 08:51:44 +0100

    Add secondary links

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -19,21 +19,36 @@ export default function Header({
 	linkToWeek = true,
 }) {
 	return (
-		<header class={classes.header}>
-			{linkToYear ? <a href={`#y${getYear(date)}`}>{getYear(date)}</a> : null}
-			{linkToQuarter ? (
-				<a href={`#q${getQuarterOfYear(date)}`}>Q{getQuarterOfYear(date)}</a>
-			) : null}
-			{linkToMonth ? (
-				<a href={`#m${getMonthOfYear(date)}`}>
-					{monthNameFormatter.format(date)}
-				</a>
-			) : null}
-			{linkToWeek ? (
-				<a href={`#w${getWeekOfYear(date)}`}>Week {getWeekOfYear(date)}</a>
-			) : null}
-			<span class={classes.title}>{titleFormatter(date)}</span>
-			<span class={classes.closebtn} />
-		</header>
+		<>
+			<header class={classes.header}>
+				{linkToYear ? (
+					<a href={`#y${getYear(date) + 1}`}>{getYear(date)}</a>
+				) : null}
+				{linkToQuarter ? (
+					<a href={`#q${getQuarterOfYear(date)}`}>Q{getQuarterOfYear(date)}</a>
+				) : null}
+				{linkToMonth ? (
+					<a href={`#m${getMonthOfYear(date)}`}>
+						{monthNameFormatter.format(date)}
+					</a>
+				) : null}
+				{linkToWeek ? (
+					<a href={`#w${getWeekOfYear(date)}`}>Week {getWeekOfYear(date)}</a>
+				) : null}
+				<span class={classes.title}>{titleFormatter(date)}</span>
+				<span class={classes.closebtn} />
+			</header>
+			<header class={[classes.header, classes.forwardlinks].join(" ")}>
+				{linkToQuarter && getQuarterOfYear(date) < 4 ? (
+					<a href={`#q${getQuarterOfYear(date) + 1}`}>Next Quarter</a>
+				) : null}
+				{linkToMonth && getMonthOfYear(date) < 12 ? (
+					<a href={`#m${getMonthOfYear(date) + 1}`}>Next Month</a>
+				) : null}
+				{linkToWeek ? (
+					<a href={`#w${getWeekOfYear(date) + 1}`}>Next Week</a>
+				) : null}
+			</header>
+		</>
 	);
 }
